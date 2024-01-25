66616549fbab57175686755589781e605bc4a9f9
Facundo Elias <facundo.elias10@gmail.com>
2024-01-24 21:27:39 -0300

    fetchApi

--- src/components/ItemsContainer.jsx
+++ src/components/ItemsContainer.jsx
@@ -1,12 +1,48 @@
-import ItemListContainer from "./ItemListContainer";
+
+import { useState, useEffect } from "react"
+import data from "../../data/tiendOnProducts.json"
 
 const ItemsContainer = () => {
-    return (
-        <div className="col-md-4">
-            <ItemListContainer cardTitle={"Producto 1"} cardText={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda iure quidem itaque illo dolorem laudantium pariatur nesciunt earum."}/>
+
+    const [data, setData] = useState([])
+ 
+    useEffect (() =>{
+        fetch("../../data/tiendOnProducts.json")
+        .then((resp) => resp.json())
+        .then((data) => {
+            setData(data)
+        })
+    },[])
+
+    
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <h1>Productos</h1>
+                </div>
+            </div>
+            <div className="row ">
+                {data.map((data) =>(
+                    <div className="col-md-3 my-2" key={data.id}>
+                        <div className="card my-2 h-100">
+                            <img src= {data.image} className="card-img-top" alt={data.titulo}/>
+                            <div className="card-body">
+                                <h5 className="card-title">{data.nombre}</h5>
+                                <div className="container  ">
+                                <a href="#" className="btn btn-primary m-1 ">{data.precio}</a>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                ))}
+            </div>             
         </div>
     )
 }
 
-export default ItemsContainer
 
+
+
+export default ItemsContainer;
