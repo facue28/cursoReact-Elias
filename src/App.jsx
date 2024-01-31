a61687ac0ce6db6e5d6299fc30dce38842f59434
Facundo Elias <facundo.elias10@gmail.com>
2024-01-30 21:56:04 -0300

    arregloNav

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 
 
 const App = () => {
   return (
       <>
       <BrowserRouter>
-        <NavBar/>
+          <Header/>
           <Routes>
             <Route path="/" element= {<ItemListContainer />}/>
+            <Route path="category/:id" element= {<ItemListContainer />}/>
             <Route path="/item/:id" element={<ItemDetailContainer/>} />
           </Routes>
       </BrowserRouter>
